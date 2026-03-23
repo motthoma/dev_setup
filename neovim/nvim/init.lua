260a033f9d42a238d77081ed9c9efebcb5c56b1e
legmot0 <thomas.motz@liebherr.com>
2026-03-24 00:19:00 +0100

    autocompletion finished and made consistent with cmp.

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -906,7 +906,7 @@ require('lazy').setup({
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
 
-        preset = 'default',
+        preset = 'enter',
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
@@ -922,6 +922,7 @@ require('lazy').setup({
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
         documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        list = { selection = { preselect = true, auto_insert = true } },
       },
 
       sources = {
