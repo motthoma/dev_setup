ad92af19155cc45950c70a63c4b473348387ea85
legmot0 <thomas.motz@liebherr.com>
2026-03-25 15:50:31 +0100

    init.lua changed to use enter as command to accept first suggestion.

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -906,7 +906,8 @@ require('lazy').setup({
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
 
-        preset = 'default',
+        preset = 'enter',
+        -- preset = 'default',
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
